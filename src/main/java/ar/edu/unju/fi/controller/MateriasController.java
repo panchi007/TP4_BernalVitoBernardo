d5a604a7149baef0aa39a352de2468aa6d6209e3
panchi007 <vitobernal@hotmail.com>
2024-06-08 20:36:40 -0300

    Trabajo Practico Numero Cuatro Agregando Parte 2 correccion en template listaDeMaterias, formMmaterias y en controller

--- src/main/java/ar/edu/unju/fi/controller/MateriasController.java
+++ src/main/java/ar/edu/unju/fi/controller/MateriasController.java
@@ -23,87 +23,82 @@ import ar.edu.unju.fi.model.Materia;
 @Controller
 public class MateriasController {
 
-	@InitBinder
-	public void initBinder(WebDataBinder binder) {
-		binder.registerCustomEditor(Docente.class, new PropertyEditorSupport() {
-			@Override
-			public void setAsText(String id) {
-				Docente docente = DocenteCollection.listarDocentes().stream().filter(d -> d.getId().equals(id))
-						.findFirst().orElse(null);
-				setValue(docente);
-			}
-		});
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(Docente.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String id) {
+                Docente docente = DocenteCollection.listarDocentes().stream().filter(d -> d.getId().equals(id))
+                        .findFirst().orElse(null);
+                setValue(docente);
+            }
+        });
 
-		binder.registerCustomEditor(Carrera.class, new PropertyEditorSupport() {
-			@Override
-			public void setAsText(String id) {
-				Carrera carrera = CarreraCollection.listarCarreras().stream().filter(c -> c.getCodigo().equals(id))
-						.findFirst().orElse(null);
-				setValue(carrera);
-			}
-		});
-	}
+        binder.registerCustomEditor(Carrera.class, new PropertyEditorSupport() {
+            @Override
+            public void setAsText(String id) {
+                Carrera carrera = CarreraCollection.listarCarreras().stream().filter(c -> c.getCodigo().equals(id))
+                        .findFirst().orElse(null);
+                setValue(carrera);
+            }
+        });
+    }
 
-	@GetMapping("/listaDeMaterias")
-	public ModelAndView listarMaterias() {
-		List<Materia> materias = MateriaCollection.listarMaterias();
-		ModelAndView modelView = new ModelAndView("listaDeMaterias");
-		modelView.addObject("materiaListado", materias);
-		return modelView;
-	}
+    @GetMapping("/listaDeMaterias")
+    public ModelAndView listarMaterias() {
+        List<Materia> materias = MateriaCollection.listarMaterias();
+        ModelAndView modelView = new ModelAndView("listaDeMaterias");
+        modelView.addObject("materiaListado", materias);
+        return modelView;
+    }
 
-	@GetMapping("/formularioMateria")
-	public ModelAndView getFormMateria() {
-		ModelAndView modelView = new ModelAndView("formMaterias");
-		modelView.addObject("nuevaMateria", new Materia());
-		modelView.addObject("docentes", DocenteCollection.listarDocentes());
-		modelView.addObject("carreras", CarreraCollection.listarCarreras());
-		return modelView;
-	}
+    @GetMapping("/formularioMateria")
+    public ModelAndView getFormMateria() {
+        ModelAndView modelView = new ModelAndView("formMaterias");
+        modelView.addObject("nuevaMateria", new Materia());
+        modelView.addObject("docentes", DocenteCollection.listarDocentes());
+        modelView.addObject("carreras", CarreraCollection.listarCarreras());
+        return modelView;
+    }
 
-	@GetMapping("/editarMateria/{codigo}")
-	public ModelAndView getFormEditarMateria(@PathVariable String codigo) {
-		Materia materiaParaModificar = MateriaCollection.buscarMateriaPorCodigo(codigo);
-		if (materiaParaModificar == null) {
-			return new ModelAndView("redirect:/listaDeMaterias");
-		}
-		ModelAndView modelView = new ModelAndView("formMaterias");
-		modelView.addObject("nuevaMateria", materiaParaModificar);
-		modelView.addObject("docentes", DocenteCollection.listarDocentes());
-		modelView.addObject("carreras", CarreraCollection.listarCarreras());
-		return modelView;
-	}
+    @GetMapping("/editarMateria/{codigo}")
+    public ModelAndView getFormEditarMateria(@PathVariable String codigo) {
+        Materia materiaParaModificar = MateriaCollection.buscarMateriaPorCodigo(codigo);
+        if (materiaParaModificar == null) {
+            return new ModelAndView("redirect:/listaDeMaterias");
+        }
+        ModelAndView modelView = new ModelAndView("formMaterias");
+        modelView.addObject("nuevaMateria", materiaParaModificar);
+        modelView.addObject("docentes", DocenteCollection.listarDocentes());
+        modelView.addObject("carreras", CarreraCollection.listarCarreras());
+        return modelView;
+    }
 
-	@PostMapping("/guardarMateria")
-	public String saveMateria(@ModelAttribute("nuevaMateria") Materia materiaParaGuardar,
-			RedirectAttributes redirectAttributes) {
-		MateriaCollection.agregarMateria(materiaParaGuardar);
-		redirectAttributes.addFlashAttribute("mensaje", "La materia ha sido agregada exitosamente.");
-		return "redirect:/listaDeMaterias";
-	}
+    @PostMapping("/guardarMateria")
+    public String saveMateria(@ModelAttribute("nuevaMateria") Materia materiaParaGuardar,
+            RedirectAttributes redirectAttributes) {
+        MateriaCollection.agregarMateria(materiaParaGuardar);
+        redirectAttributes.addFlashAttribute("mensaje", "La materia ha sido agregada exitosamente.");
+        return "redirect:/listaDeMaterias";
+    }
 
-	@PostMapping("/modificarMateria")
-	public String saveMateriaModificada(@ModelAttribute("nuevaMateria") Materia materiaModificada,
-			RedirectAttributes redirectAttributes) {
-		System.out.println("Datos recibidos para modificar: " + materiaModificada); // Línea de depuración
-		Materia materiaExistente = MateriaCollection.buscarMateriaPorCodigo(materiaModificada.getCodigo());
-		if (materiaExistente != null) {
-			materiaExistente.setNombre(materiaModificada.getNombre());
-			materiaExistente.setCurso(materiaModificada.getCurso());
-			materiaExistente.setCantidadHoras(materiaModificada.getCantidadHoras());
-			materiaExistente.setModalidad(materiaModificada.getModalidad());
-			materiaExistente.setDocente(materiaModificada.getDocente());
-			materiaExistente.setCarrera(materiaModificada.getCarrera());
-			MateriaCollection.modificarMateria(materiaExistente);
-		}
-		redirectAttributes.addFlashAttribute("mensaje", "La materia ha sido modificada exitosamente.");
-		return "redirect:/listaDeMaterias";
-	}
+    @PostMapping("/modificarMateria")
+    public String saveMateriaModificada(@ModelAttribute("nuevaMateria") Materia materiaModificada, RedirectAttributes redirectAttributes) {
+        System.out.println("Datos recibidos para modificar: " + materiaModificada); // Línea de depuración
+        if (materiaModificada.getCodigo() == null) {
+            System.out.println("Código es nulo, revisa el formulario.");
+        }
+        MateriaCollection.modificarMateria(materiaModificada);
+        redirectAttributes.addFlashAttribute("mensaje", "La materia ha sido modificada exitosamente.");
+        return "redirect:/listaDeMaterias";
+    }
 
-	@GetMapping("/borrarMateria/{codigo}")
-	public String deleteMateria(@PathVariable String codigo, RedirectAttributes redirectAttributes) {
-		MateriaCollection.eliminarMateria(codigo);
-		redirectAttributes.addFlashAttribute("mensaje", "La materia ha sido eliminada exitosamente.");
-		return "redirect:/listaDeMaterias";
-	}
+
+
+    @GetMapping("/borrarMateria/{codigo}")
+    public String deleteMateria(@PathVariable String codigo, RedirectAttributes redirectAttributes) {
+        MateriaCollection.eliminarMateria(codigo);
+        redirectAttributes.addFlashAttribute("mensaje", "La materia ha sido eliminada exitosamente.");
+        return "redirect:/listaDeMaterias";
+    }
 }
